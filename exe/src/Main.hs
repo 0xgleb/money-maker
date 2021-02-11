4712a6e30a83d04330e91c7f758c4c20ce44a0a5
Gleb Dianov <gleb.dianov@protonmail.com>
2021-02-11 23:31:35 +0000

    connected process wip

--- exe/src/Main.hs
+++ exe/src/Main.hs
@@ -2,9 +2,11 @@ module Main where
 
 import Protolude
 
-import qualified System.Process as Proc
 import qualified Control.Concurrent.STM as STM
-import qualified System.IO as IO
+import qualified Data.Text.Lazy.IO      as Txt.LIO
+import qualified Paths_exe              as Path
+import qualified System.IO              as IO
+import qualified System.Process         as Proc
 
 main :: IO ()
 main = do
@@ -17,22 +19,33 @@ main = do
     prediction <- STM.atomically $ STM.readTChan predictionChannel
     handlePrediction prediction
 
-  void $ Proc.waitForProcess processHandle
+  void $ Proc.terminateProcess processHandle
   where
     -- placeholder for the function that will get live price data from the
     -- Binance Websockets API, process it, and write relevant information
     -- into the price data channel
-    getLivePriceData _priceDataChannel = undefined
+    getLivePriceData :: STM.TChan ContractualPriceData -> IO ()
+    getLivePriceData priceDataChannel = do
+      message <- getLine
+      STM.atomically $ STM.writeTChan priceDataChannel ContractualPriceData{..}
 
     -- placeholder for the function that will take a new prediction from the
     -- prediction process and evaluate whether it needs to make any changes
     -- to the portfolio based on that
-    handlePrediction _prediction = undefined
+    handlePrediction ContractualPrediction{..} = do
+      putStrLn $ "Got back: " <> message
 
 -- I think using "Contractual" prefix can help identify which types have to have
 -- a certain encoding to not break the contract with the prediction mechanism
-data ContractualPriceData = ContractualPriceData {}
-data ContractualPrediction = ContractualPrediction {}
+data ContractualPriceData
+  = ContractualPriceData
+      { message :: Text
+      }
+
+data ContractualPrediction
+  = ContractualPrediction
+      { message :: LText
+      }
 
 spawnChannelledPredictionProcess
   :: IO (STM.TChan ContractualPriceData, STM.TChan ContractualPrediction, Proc.ProcessHandle)
@@ -40,27 +53,28 @@ spawnChannelledPredictionProcess = do
   priceDataChannel <- STM.newTChanIO
   predictionsChannel <- STM.newTChanIO
 
+  scriptPath <- Path.getDataFileName "../soothsayer/example.py"
+
   (fmap InputHandle -> Just inputHandle
     , fmap OutputHandle -> Just outputHandle
     , _
     , processHandle
-    ) <- Proc.createProcess processDescription
+    ) <- Proc.createProcess $ processDescription scriptPath
 
-  -- hSetBuffering inputHandle NoBuffering
-  -- hSetBuffering outputHandle NoBuffering
+  IO.hSetBuffering (unpackInputHandle inputHandle) IO.NoBuffering
+  IO.hSetBuffering (unpackOutputHandle outputHandle) IO.NoBuffering
 
   -- need a separate thread to run the infinite loop
   void $ forkIO $ infinitePriceChannelToInputLink priceDataChannel inputHandle
 
   -- need a separate thread to run the infinite loop
-  void $ forkIO $ infiniteOutputToPredictionsChannelLink outputHandle predictionsChannel
+  void $ forkIO $ infiniteOutputToPredictionsChannelLink outputHandle priceDataChannel predictionsChannel
 
   pure (priceDataChannel, predictionsChannel, processHandle)
 
   where
-    pythonFile = undefined
-    processDescription
-      = (Proc.proc ("python " <> pythonFile) [])
+    processDescription scriptPath
+      = (Proc.proc "/usr/bin/python" [scriptPath])
           { Proc.std_in = Proc.CreatePipe, Proc.std_out = Proc.CreatePipe }
 
 newtype InputHandle
@@ -76,10 +90,10 @@ infinitePriceChannelToInputLink
   -> InputHandle
   -> IO ()
 infinitePriceChannelToInputLink priceDataChannel inputHandle = do
-  _priceData <- STM.atomically $ STM.readTChan priceDataChannel
-  let shouldStop = False -- just a placeholder
+  ContractualPriceData{..} <- STM.atomically $ STM.readTChan priceDataChannel
+  let shouldStop = message == "stop"
   unless shouldStop $ do
-    hPutStrLn @ByteString (unpackInputHandle inputHandle) "just a test"
+    hPutStrLn @Text (unpackInputHandle inputHandle) message
     infinitePriceChannelToInputLink priceDataChannel inputHandle
 
 newtype OutputHandle
@@ -90,14 +104,18 @@ newtype OutputHandle
 -- prediction in the output
 infiniteOutputToPredictionsChannelLink
   :: OutputHandle
+  -> STM.TChan ContractualPriceData
   -> STM.TChan ContractualPrediction
   -> IO ()
 infiniteOutputToPredictionsChannelLink
   outputHandle@(OutputHandle unpackedOutputHandle)
+  priceChannel
   predictionsChannel
   = do
+
   closed <- IO.hIsClosed unpackedOutputHandle
   unless closed $ do
-    _prediction <- IO.hGetLine unpackedOutputHandle -- maybe `catch` exceptions???
-    STM.atomically $ STM.writeTChan predictionsChannel ContractualPrediction{}
-    infiniteOutputToPredictionsChannelLink outputHandle predictionsChannel
+    -- message <- IO.hGetLine unpackedOutputHandle
+    message <- Txt.LIO.hGetLine unpackedOutputHandle
+    STM.atomically $ STM.writeTChan predictionsChannel ContractualPrediction{..}
+    infiniteOutputToPredictionsChannelLink outputHandle priceChannel predictionsChannel
