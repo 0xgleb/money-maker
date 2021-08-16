177454a3448e1a4e7e3c5d052c82b66cb3fc91ea
Gleb Dianov <gleb.dianov@protonmail.com>
2021-08-16 19:34:08 +0400

    create untested swing event

--- exe/src/Contract.hs
+++ exe/src/Contract.hs
@@ -5,6 +5,7 @@ module Contract
   , toContractualPriceData
 
   , ContractualPrediction(..)
+  , Price(..)
   )
   where
 
@@ -14,6 +15,7 @@ import qualified MoneyMaker.Eventful                as Eventful
 import Protolude
 
 import qualified Data.Aeson as Aeson
+import qualified Data.Fixed as Fixed
 
 -- I think using "Contractual" prefix can help identify which types have to have
 -- a certain encoding to not break the contract with the prediction mechanism
@@ -25,11 +27,16 @@ data ContractualPriceData
   deriving stock (Eq, Show, Generic)
   deriving anyclass (Aeson.ToJSON)
 
+newtype Price
+  = Price { getPrice :: Fixed.Centi }
+  deriving newtype (Show, Eq, Ord, Aeson.ToJSON, Aeson.FromJSON)
+
 toContractualPriceData
   :: Eventful.MonadEventStore m
   => Coinbase.TickerPriceData
   -> m errors ContractualPriceData
 toContractualPriceData Coinbase.TickerPriceData{..} = do
+  -- TODO: consider max precision
   pure ContractualPriceData{..}
 
 -- Just a placeholder until Python actually sends some useful data
@@ -37,3 +44,92 @@ data ContractualPrediction
   = ContractualPrediction
       { message :: LText
       }
+
+
+-- data SwingCommand
+--   = AddNewHigh Price
+--   | AddNewLow Price
+
+-- instance Eventful.Command SwingCommand SwingEvent where
+--   type CommandErrors SwingCommand = '[]
+
+--   handleCommand pairId
+
+
+data SwingEvent
+  = NewHighReached Price
+  | NewLowReached Price
+  deriving stock (Generic)
+  deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
+
+data Swings
+  = SwingUp High
+  | SwingDown Low
+
+data High
+  = High
+      { price       :: Price
+      , previousLow :: Maybe Low
+      }
+
+data Low
+  = Low
+      { price        :: Price
+      , previousHigh :: Maybe High
+      }
+
+instance Eventful.Eventful SwingEvent where
+  type EventName      SwingEvent = "swing"
+  type EventAggregate SwingEvent = Swings
+  type EventError     SwingEvent = SwingEvent
+
+  applyEvent Nothing event = pure $ case event of
+    NewHighReached price ->
+      SwingUp $ High price Nothing
+    NewLowReached price ->
+      SwingDown $ Low price Nothing
+
+  applyEvent (Just aggregate) event
+    = pure $ case (aggregate, event) of
+        ( SwingUp High{previousLow}, NewHighReached price ) ->
+          SwingUp High{..}
+
+        ( SwingDown Low{previousHigh}, NewLowReached price ) ->
+          SwingDown Low{..}
+
+        ( SwingDown low, NewHighReached price ) ->
+          SwingUp $ addNewHigh price low
+
+        ( SwingUp high, NewLowReached price ) ->
+          SwingDown $ addNewLow price high
+
+
+addNewHigh :: Price -> Low -> High
+addNewHigh newHighPrice low@Low{previousHigh}
+  = case previousHigh of
+      Nothing ->
+        High newHighPrice $ Just low
+
+      Just High{price = prevHighPrice} | newHighPrice < prevHighPrice ->
+        High newHighPrice $ Just low
+
+      Just high ->
+        maybe
+          (High newHighPrice $ Just low)
+          (addNewHigh newHighPrice)
+          (previousLow high)
+
+addNewLow :: Price -> High -> Low
+addNewLow newLowPrice high@High{previousLow}
+  = case previousLow of
+      Nothing ->
+        Low newLowPrice $ Just high
+
+      Just Low{price = prevLowPrice} | newLowPrice > prevLowPrice ->
+        Low newLowPrice $ Just high
+
+      Just low ->
+        maybe
+          (Low newLowPrice $ Just high)
+          (addNewLow newLowPrice)
+          (previousHigh low)
