1430b2385b0d7b7316059dddeec1b6f67f10926f
Gleb Dianov <gleb.dianov@protonmail.com>
2021-08-31 05:24:28 +0400

    wip candles adoption

--- lib/price-preprocessor/src/MoneyMaker/PricePreprocessor.hs
+++ lib/price-preprocessor/src/MoneyMaker/PricePreprocessor.hs
@@ -6,14 +6,11 @@ module MoneyMaker.PricePreprocessor
 
   , ContractualPrediction(..)
 
-  , SwingCommand(..)
-  , SwingEvent
-  , Swings(..)
-  , getLastPrice
-  , High(..)
-  , Low(..)
+  , module Swings
   )
   where
+
+import MoneyMaker.PricePreprocessor.Swings as Swings
 
 import qualified MoneyMaker.Coinbase.SDK as Coinbase
 import qualified MoneyMaker.Error        as Error
@@ -21,8 +18,9 @@ import qualified MoneyMaker.Eventful     as Eventful
 
 import Protolude
 
-import qualified Data.Aeson      as Aeson
-import qualified Data.Time.Clock as Time
+import qualified Data.Aeson            as Aeson
+import qualified Data.Generics.Product as Generics
+import qualified Data.Time.Clock       as Time
 
 -- I think using "Contractual" prefix can help identify which types have to have
 -- a certain encoding to not break the contract with the prediction mechanism
@@ -40,137 +38,46 @@ toContractualPriceData
   :: ( Eventful.MonadEventStore m
      , Eventful.CouldntDecodeEventError `Error.Elem` errors
      , Eventful.NoEventsFoundError `Error.Elem` errors
+     , MonadIO (m errors)
      )
   => Coinbase.TickerPriceData
   -> m errors ContractualPriceData
 toContractualPriceData Coinbase.TickerPriceData{..} = do
   let id = Eventful.Id [Eventful.uuid|123e4567-e89b-12d3-a456-426614174000|]
+      -- just a random ID
 
-  -- I tried running this with live data and I think we're better off using candles
-  -- for calculating swings. Otherwise we will emit an event for every single price movement,
-  -- which will spam the database very significantly.
-  --
-  -- We can't get candles from the websockets API, so REST API support needs to be added first
-  swings <-
+  currentTime <- liftIO Time.getCurrentTime
+
+  savedSwings <-
     Error.catchVoid
-      $ Eventful.applyCommand id
-      $ AddNewPrice (Coinbase.Price price) time
+      $ Eventful.getAggregate @SwingEvent id
+
+  let timeOfPreviousSave
+        = Generics.getField @"time" $ getLastPrice savedSwings
+
+      timeSinceLastSavedPrice
+        = Time.diffUTCTime timeOfPreviousSave currentTime
+
+  swings <-
+    if timeSinceLastSavedPrice <= 60
+    then pure savedSwings
+    else do
+      candles <-
+        Coinbase.getCandles productId timeOfPreviousSave currentTime
+          Coinbase.OneMinute
+
+      convertCandlesToPriceUpdates
+
+      -- Error.catchVoid
+      --   $ Eventful.applyCommand id
+      --   $ AddNewPrice (Coinbase.Price price) time
 
   pure ContractualPriceData{ price = Coinbase.Price price, ..}
+
+
 
 -- Just a placeholder until Python actually sends some useful data
 data ContractualPrediction
   = ContractualPrediction
       { message :: LText
       }
-
-
-data SwingCommand
-  = AddNewPrice Coinbase.Price Time.UTCTime
-
-instance Eventful.Command SwingCommand SwingEvent where
-  type CommandErrors SwingCommand = '[]
-
-  handleCommand _id Nothing (AddNewPrice price time)
-    = pure $ NewLowReached price time :| []
-
-  handleCommand _id (Just swings) (AddNewPrice newPrice time)
-    = let previousPrice = case swings of
-            SwingUp   High{price} -> price
-            SwingDown Low{price}  -> price
-
-      in pure $ if newPrice > previousPrice
-            then NewHighReached newPrice time :| []
-            else NewLowReached newPrice time :| []
-
-
-data SwingEvent
-  = NewHighReached Coinbase.Price Time.UTCTime
-  | NewLowReached Coinbase.Price Time.UTCTime
-  deriving stock (Generic)
-  deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
-
-data Swings
-  = SwingUp High
-  | SwingDown Low
-  deriving stock (Eq, Show, Generic)
-  deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
-
-getLastPrice :: Swings -> Coinbase.Price
-getLastPrice = \case
-  SwingUp High{..}  -> price
-  SwingDown Low{..} -> price
-
-data High
-  = High
-      { price       :: Coinbase.Price
-      , time        :: Time.UTCTime
-      , previousLow :: Maybe Low
-      }
-  deriving stock (Eq, Show, Generic)
-  deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
-
-data Low
-  = Low
-      { price        :: Coinbase.Price
-      , time         :: Time.UTCTime
-      , previousHigh :: Maybe High
-      }
-  deriving stock (Eq, Show, Generic)
-  deriving anyclass (Aeson.ToJSON, Aeson.FromJSON)
-
-instance Eventful.Eventful SwingEvent where
-  type EventName      SwingEvent = "swing"
-  type EventAggregate SwingEvent = Swings
-  type EventError     SwingEvent = Void
-
-  applyEvent Nothing event = pure $ case event of
-    NewHighReached price time ->
-      SwingUp $ High price time Nothing
-    NewLowReached price time ->
-      SwingDown $ Low price time Nothing
-
-  applyEvent (Just aggregate) event
-    = pure $ case (aggregate, event) of
-        ( SwingUp High{previousLow}, NewHighReached price time ) ->
-          SwingUp High{..}
-
-        ( SwingDown Low{previousHigh}, NewLowReached price time ) ->
-          SwingDown Low{..}
-
-        ( SwingDown low, NewHighReached price time ) ->
-          SwingUp $ addNewHigh price time low
-
-        ( SwingUp high, NewLowReached price time ) ->
-          SwingDown $ addNewLow price time high
-
-
-addNewHigh :: Coinbase.Price -> Time.UTCTime -> Low -> High
-addNewHigh newHighPrice time low@Low{previousHigh}
-  = case previousHigh of
-      Nothing ->
-        High newHighPrice time $ Just low
-
-      Just High{price = prevHighPrice} | newHighPrice < prevHighPrice ->
-        High newHighPrice time $ Just low
-
-      Just high ->
-        maybe
-          (High newHighPrice time $ Just low)
-          (addNewHigh newHighPrice time)
-          (previousLow high)
-
-addNewLow :: Coinbase.Price -> Time.UTCTime -> High -> Low
-addNewLow newLowPrice time high@High{previousLow}
-  = case previousLow of
-      Nothing ->
-        Low newLowPrice time $ Just high
-
-      Just Low{price = prevLowPrice} | newLowPrice > prevLowPrice ->
-        Low newLowPrice time $ Just high
-
-      Just low ->
-        maybe
-          (Low newLowPrice time $ Just high)
-          (addNewLow newLowPrice time)
-          (previousHigh low)
